833b384947a9bd73509a655a86486679f9f3444e
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-05-25 10:14:03 +0300

    second test

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+console.log('test console')
 
 app.listen(3000, () => console.log(`App is runnging on ${PORT}`));
