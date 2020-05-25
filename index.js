832a80bf26fa6a8f022800b1a06892e1bb263ecf
Pavlo Vasylkivskyi <Pavlo@MacBook-Pro.local>
2020-05-25 10:08:55 +0300

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log(`App is runnging on ${PORT}`));
